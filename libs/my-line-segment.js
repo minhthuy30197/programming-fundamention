bbd64cda15b6af15fdf74f8ce172038382a0074a
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-06 16:00:54 +0700

    my-line-segment

--- libs/my-line-segment.js
+++ libs/my-line-segment.js
@@ -3,7 +3,12 @@ function createLine(len) {
   return {
     lenght:len,
     print: function() {
-      let rs = '*****';
+      let rs = '';
+      let tmp = this.lenght;
+      while (tmp > 0) {
+        rs += '*';
+        tmp--;
+      }
       return rs;
     }
   };
