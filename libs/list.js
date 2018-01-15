bb13302ff606a818bdd72bf0c1e592ee5507f8c7
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-15 16:57:44 +0700

    new list library

--- libs/list.js
+++ libs/list.js
@@ -1,16 +1,16 @@
 'use strict'
-function list() {
+function list () {
   return {
-    length:0,
+    length: 0,
     push: function(obj) {
-      this[this.length] = obj;
-      this.length++;
+      this[this.length] = obj
+      this.length++
     },
     pop: function() {
-      this.length--;
-      let tmp = this[this.length];
-      this[this.length] = undefined;
-      return tmp;
+      this.length--
+      let tmp = this[this.length]
+      this[this.length] = undefined
+      return tmp
     }
   }
 }
